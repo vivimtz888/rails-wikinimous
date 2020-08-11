9d28f50f995d863d3ea78ba9ffe3adf6f6ab205b
Viviana Martinez <viviana.mmontesdeoca@gmail.com>
2020-08-11 17:47:33 -0500

    refactor of the controllers

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,12 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end
 
     def new
@@ -18,15 +20,31 @@ class ArticlesController < ApplicationController
         redirect_to article_path(@article)
     end
 
+    def edit
+        # @article = Article.find(params[:id])
+    end
+
+    def update
+        # @article = Article.find(params[:id])
+        @article.update(article_params)
+        redirect_to articles_path
+    end
+
     def destroy
-        @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
         @article.destroy
+
+        redirect_to articles_path
     end
 
     private
 
     def article_params
         params.require(:article).permit(:title, :content)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
     end
 
 
